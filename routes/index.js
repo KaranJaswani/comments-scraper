4f00fde2d8edd39c5c390ccd34cb0fcad33a9cdd
Karan Jaswani <karanjaswani123@gmail.com>
2017-09-20 23:40:22 -0500

    including youtube scraper package

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var scraper = require("youtube-comment-scraper");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.post('/get-comments', function(req, res, next) {
+
+  var response;
+  scraper.comments(req.body.url).then(function(rsp) {
+    response = rsp;
+    console.log(rsp);
+    // Close scraper.
+    scraper.close();
+  });
+
+  res.render('comments', {comments: response});
 });
 
 module.exports = router;
